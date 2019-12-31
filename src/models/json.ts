9ecd057ffd5f88756ce0b85b058016ac8cab7965
iChrisx10x <christopherx10x@gmail.com>
2019-12-30 20:31:40 -0600

    objectID inner tripList remove

--- src/models/json.ts
+++ src/models/json.ts
@@ -1,26 +1,32 @@
 import { Schema, model } from 'mongoose';
+import { ObjectId } from 'mongodb';
+
+const TripSchema = new Schema({
+    TripID: String,
+    Destination:String,
+    Position: {
+        Timestamp:Number,
+        Train:String,
+        Lat: Number,
+        Long: Number,
+        Heading:Number
+    },
+    Predictions: {
+        StopID: String,
+        Stop:String,
+        Seconds: Number
+    }
+},{ _id : false })
 
 const JsonSchema = new Schema({
     TripList: {
+        trainID: ObjectId,
         CurrentTime: Number,
         Line: String,
-        Trips: [{
-            TripID: String,
-            Destination:String,
-            Position: {
-                Timestamp:Number,
-                Train:String,
-                Lat: Number,
-                Long: Number,
-                Heading:Number
-            },
-            Predictions: {
-                StopID: String,
-                Stop:String,
-                Seconds: Number
-            }
-        }]
+        Trips: [TripSchema]
     }
 })
+ 
+
 
 export default model('Json', JsonSchema);
