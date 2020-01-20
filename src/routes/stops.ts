c3485b13b8fe7c81a6c56ad7f3e49a99c2f2594a
iChrisx10x <christopherx10x@gmail.com>
2020-01-19 23:59:33 -0600

    get red, blue and orange stops endpoint

--- src/routes/stops.ts
+++ src/routes/stops.ts
@@ -8,12 +8,30 @@ import Stops from "../models/Stops";
 
 //GET TASKS AS JSON
 router.route('/blue')
-        .get(
-            async (req: Request, res: Response) => {
-                const stopsList = await Stops.findOne();
-                console.log(stopsList);
-                return res.status(200).json(stopsList);
-            }
-        );
+    .get(
+        async (req: Request, res: Response) => {
+            const stopsList = await Stops.find({ 'properties.lines': "blue" });
+            console.log(stopsList);
+            return res.status(200).json(stopsList);
+        }
+    );
+
+router.route('/red')
+    .get(
+        async (req: Request, res: Response) => {
+            const stopsList = await Stops.find({ 'properties.lines': "red" });
+            console.log(stopsList);
+            return res.status(200).json(stopsList);
+        }
+    );
+
+router.route('/orange')
+    .get(
+        async (req: Request, res: Response) => {
+            const stopsList = await Stops.find({ 'properties.lines': "orange" });
+            console.log(stopsList);
+            return res.status(200).json(stopsList);
+        }
+    );
 
 export default router;
