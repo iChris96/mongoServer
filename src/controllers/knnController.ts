619645513773ee8efa14ac475113c32d3bec0354
iChrisx10x <christopherx10x@gmail.com>
2020-02-23 23:38:57 -0600

    using EntriesKNN collection with Knn

--- src/controllers/knnController.ts
+++ src/controllers/knnController.ts
@@ -1,51 +1,58 @@
 import { Request, Response } from 'express';
-import { Decimal128 } from 'bson';
 const KNN = require('@artificialscience/k-nn');
 const nn = require('nearest-neighbor');
-import EntriesKNN from '../models/EntriesKNN';
+import EntriesKNN, { IEntriesKnn } from '../models/EntriesKNN';
 
-interface instancia {
-	x: number;
-	y: number;
-	z: number;
+interface data {
+	entries: number;
+	day: number;
+	hour: number;
+	minutes: number;
 }
 
 class KnnController {
 	public async test(req: Request, res: Response) {
-		//https://www.npmjs.com/package/@artificialscience/k-nn
-		/*const features = [
-			[10, 0.5068958512233102, 16, 1],
-			[12, 0.5105356367730379, 16, 7],
-			[12, 0.5323562633463617, 16, 7],
-			[40, 0.517546804614676, 16, 2],
-			[48, 0.5158030616769878, 16, 2],
-			[83, 0.5262942735596369, 16, 6],
-			[80, 0.5362942735596369, 16, 6],
-			[97, 0.5281527464327415, 16, 100],
-			[98, 0.5148717143151224, 16, 100],
-			[99, 0.5248717143151224, 16, 100],
-			[100, 0.5148717143151224, 16, 100],
-			[98, 0.5448717143151224, 16, 100],
-			[98, 0.5648717143151224, 16, 100],
-			[94, 0.5148717143151224, 16, 100],
-			[98, 0.52148717143151224, 16, 100]
-        ];*/
+		/*let entries = await EntriesKNN.find(
+			{ station: 'Andrew Square' }
+        );*/
+
+		//CALL ENTRIES BY STATION
+		/*let entries = await EntriesKNN.find(
+			{ station: 'Andrew Square' },
+			'-_id day hour minutes entries',
+			function(err, entity) {
+				//console.log(entity); // { field1: '...', field2: '...' }
+			}
+        );*/
+		let entries = await EntriesKNN.aggregate([
+			{
+				$project: {
+					_id: 0,
+					day: 1,
+					hour: 1,
+					minutes: 1,
+					entries: 1
+				}
+			}
+		]);
+
+		console.log('Entries count: ', entries.length);
+
+		//GET FIRST 15 ENTRIES
+		let first15Entries: Array<IEntriesKnn> = entries.slice(0, 15);
+		console.log('First15 count: ', first15Entries.length);
+		//console.log(first15Entries[0]);
+		//console.log(first15Entries);
+
+		let features: Array<Array<Number>> = first15Entries.map(item => {
+			//console.log(item);
+			//console.log(Object.values(item.toObject()));
+			return [item.day, item.hour, item.minutes, item.entries];
+		});
+
+		console.log(features);
 
 		/*const features = [
-			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.5, 16, 5],
-			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.9, 16, 5],
-			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.8, 16, 40],
-			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.6, 16, 12],
-			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.9, 16, 8],
-			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.11, 16, 4],
-			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.7, 16, 9]
-        ];
-        */
-
-		let entries = await EntriesKNN.find({ station: 'Andrew Square' });
-		console.log(entries.length);
-
-		const features = [
 			[1, 0, 0, 0, 15, 23, 10, 1],
 			[1, 0, 0, 0, 28, 20, 40, 5],
 			[0, 0, 0, 1, 28, 20, 30, 30],
@@ -60,26 +67,48 @@ class KnnController {
 			[0, 1, 0, 0, 4, 15, 45, 1],
 			[0, 0, 1, 0, 6, 15, 10, 0],
 			[0, 0, 0, 1, 9, 15, 5, 1]
-		];
+        ];*/
 
-		const x = (a: Array<number>, b: Array<number>) => {
+		/*
+            [ [ 1, 1, 4, 32 ],
+        [1]   [ 0, 1, 4, 34 ],
+        [1]   [ 2, 1, 5, 10 ],
+        [1]   [ 1, 1, 5, 11 ],
+        [1]   [ 1, 1, 5, 12 ],
+        [1]   [ 1, 1, 5, 13 ],
+        [1]   [ 1, 1, 5, 14 ],
+        [1]   [ 2, 1, 5, 19 ],
+        [1]   [ 1, 1, 5, 17 ],
+        [1]   [ 2, 1, 5, 20 ],
+        [1]   [ 1, 1, 5, 22 ],
+        [1]   [ 10, 1, 5, 23 ],
+        [1]   [ 3, 1, 5, 24 ],
+        [1]   [ 5, 1, 5, 25 ],
+        [1]   [ 2, 1, 5, 26 ] ]
+        */
+
+		const euclidian = (a: Array<number>, b: Array<number>) => {
 			return (
 				a
-					.map((x, i) => Math.abs(x - b[i]) ** 2) // square the difference
+					.map((euclidian, i) => Math.abs(euclidian - b[i]) ** 2) // square the difference
 					.reduce((sum, now) => sum + now) ** // sum
 				(1 / 2)
 			);
 		};
 
 		//dimension, goalIndex, customDistanceFunction, customGoalFunction
-		const knn = new KNN(7, 7, x);
+		const knn = new KNN(3, 0, euclidian);
 		//outpts, testSize, shuffleTimes, ktimes, normalize
-		let [accuracy, k] = knn.training(features, 2, features.length, 2, true);
-		console.log(k);
-		let goal = knn.predict(features, [0, 0, 0, 1, 28, 20, 40]);
+		let [accuracy, k] = knn.training(features, 2, features.length, 2, false);
+		let goal = knn.predict(features, [10, 1, 5]);
 
 		console.log(
-			'Accuracy about: ' + accuracy + ', was predicted the goal as: ' + goal
+			'Accuracy about: ' +
+				accuracy +
+				', k: ' +
+				k +
+				', was predicted the goal as: ' +
+				goal
 		);
 
 		res.json({ amazing: 'amazing' });
--- src/models/EntriesKNN.ts
+++ src/models/EntriesKNN.ts
@@ -1,24 +1,30 @@
-import mongoose, { Schema, model, Document } from 'mongoose';
-import { type } from 'os';
-
+import mongoose, { Schema, Document } from 'mongoose';
 export interface IEntriesKnn extends Document {
 	station: String;
-	entries: Number;
 	day: Number;
 	hour: Number;
 	minutes: Number;
+	entries: Number;
 }
 
 const EntriesKnnSchema = new Schema(
 	{
 		station: { type: String, required: true, unique: false },
-		entries: { type: Number, required: true, unique: false },
 		day: { type: Number, required: true, unique: false },
 		hour: { type: Number, required: true, unique: false },
-		minutes: { type: Number, required: true, unique: false }
+		minutes: { type: Number, required: true, unique: false },
+		entries: { type: Number, required: true, unique: false }
 	},
 	{ _id: false }
 );
+
+EntriesKnnSchema.set('toObject', {
+	versionKey: false,
+	transform: (doc: any, ret: any) => {
+		delete ret.__v;
+		return ret;
+	}
+});
 
 export default mongoose.model<IEntriesKnn>(
 	'Entries',
