d41f79e8673a17dbcef4e4ed48acf36becbf7858
iChrisx10x <christopherx10x@gmail.com>
2020-02-23 19:36:59 -0600

    custom distance function

--- src/controllers/knnController.ts
+++ src/controllers/knnController.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from 'express';
 const KNN = require('@artificialscience/k-nn');
 
+interface instancia {
+	x: number;
+	y: number;
+	z: number;
+}
+
 class KnnController {
 	public test(req: Request, res: Response) {
 		/*const features = [
@@ -21,20 +27,43 @@ class KnnController {
 			[98, 0.52148717143151224, 16, 100]
         ];*/
 
+		/*const features = [
+			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.5, 16, 5],
+			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.9, 16, 5],
+			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.8, 16, 40],
+			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.6, 16, 12],
+			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.9, 16, 8],
+			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.11, 16, 4],
+			[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0.7, 16, 9]
+        ];
+        */
+
 		const features = [
-			[10, 0.5, 16, 5],
-			[10, 0.9, 16, 5],
-			[10, 0.8, 16, 40],
-			[10, 0.6, 16, 12],
-			[10, 0.9, 16, 8],
-			[20, 0.11, 16, 4],
-			[20, 0.7, 16, 9]
+			[10, 10, 10, 10, 0.5, 16, 5],
+			[10, 10, 10, 10, 0.9, 16, 5],
+			[10, 10, 10, 10, 0.8, 16, 40],
+			[10, 10, 10, 10, 0.6, 16, 12],
+			[10, 10, 10, 10, 0.9, 16, 8],
+			[14, 14, 14, 14, 0.9, 14, 80],
+			[10, 10, 10, 10, 0.11, 16, 4],
+			[10, 10, 10, 10, 0.7, 16, 9]
 		];
-		const knn = new KNN(3);
+
+		const x = (a: Array<number>, b: Array<number>) => {
+			return (
+				a
+					.map((x, i) => Math.abs(x - b[i]) ** 2) // square the difference
+					.reduce((sum, now) => sum + now) ** // sum
+				(1 / 2)
+			);
+		};
+
+		//dimension, goalIndex, customDistanceFunction, customGoalFunction
+		const knn = new KNN(6, 6, x);
 		//outpts, testSize, shuffleTimes, ktimes, normalize
 		let [accuracy, k] = knn.training(features, 1, features.length, 4, false);
 		console.log(k);
-		let goal = knn.predict(features, [10, 0.7, 16]);
+		let goal = knn.predict(features, [14, 14, 14, 14, 0.9, 14]);
 
 		console.log(
 			'Accuracy about: ' + accuracy + ', was predicted the goal as: ' + goal
