7d8f664d1d7aca9c9608e0dec6503216495172b8
christopher <christopher_x10x@hotmail.com>
2021-10-03 00:13:40 -0500

    rename database

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,14 @@
 import mongoose from 'mongoose';
 
 export default async function connect() {
-
-    try {
-        await mongoose.connect(
-            'mongodb://localhost/tutorialDB',
-            { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-        );
-        console.log('Db Connected');
-    } catch (error) {
-        console.log(`Imposible to connect to Database, Error: ${error}.`);
-    }
-    
+	try {
+		await mongoose.connect('mongodb://localhost/modularDB', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true
+		});
+		console.log('Db Connected');
+	} catch (error) {
+		console.log(`Imposible to connect to Database, Error: ${error}.`);
+	}
 }
